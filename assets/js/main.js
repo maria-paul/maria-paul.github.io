03e5a709c72f6ff556125612d27815003e0bda8c
intergalactic-mammoth <aristotelis.economides@gmail.com>
2021-11-03 10:05:25 +0100

    remove id requirement from portfolio yml

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,19 @@
 
 // Initialize all the portfolio items
 let items = {};
+{% assign id = 1 %}
 {% for item in site.data.portfolio %}
-    items[{{item.id}}] = {{item.category | jsonify }} ;
+    {% assign id = id | plus:1 %}
+    items[{{id}}] = {{item.category | jsonify }} ;
 {% endfor %}
 
 function preselectCategory(category){
     console.log(`Preselecting ${category}`);
 
-    // document.getElementById(`${category}`).click();
     categories = {{site.data.settings.portfolio-categories | jsonify}};
 
+    // Prefilter portfolio items only if the category set in 
+    // settings exists
     if (categories.includes(category)){
         filterByCategory(category);
     }
@@ -23,19 +26,12 @@ function filterByCategory(selectedCategory){
     
     console.log(`Selected category "${selectedCategory}"`);
         
-    // for (let index = 0; index < Object.keys(items).length; index++) {
-    //     let itemDiv = document.getElementById(`${index+1}`);
-
-    //     if (items[index+1] != selectedCategory) {
-    //         // Debugging
-    //         console.log(`Card ${index+1} is made from ${items[index+1]}, not ${selectedCategory}. Hiding...`);
-    //         itemDiv.style.display = "none";        
-    //     }
-    // }
 
     for (let index = 0; index < Object.keys(items).length; index++) {
         let itemDiv = document.getElementById(`${index+1}`);
 
+        // If item is in selected category set display to flex
+        // otherwise set to none
         itemDiv.style.display =
         (items[index+1] == selectedCategory ) 
           ? 'flex' 
